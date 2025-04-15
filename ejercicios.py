71de3a75e18d92afbf9f1a03b7f9266b47f6d1be
Danieldrj6 <macarenadrj@hotmail.com>
2025-04-15 15:57:33 -0400

    nuevo archivo de ejercicios

---
+++ ejercicios.py
@@ -0,0 +1,7 @@
+# Promedio de 3 numeros
+
+n1 = float(input("Escribe el primer numero: "))
+n2 = float(input("Escribe el segundo numero: "))
+n3 = float(input("Escribe el tercer numero: "))
+promedio = (n1 + n2 + n3) / 3
+print("Tu promedio es: ", promedio)
