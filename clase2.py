96a14878cba7d81d183125b4c1b83c0adb4d39c0
Danieldrj6 <macarenadrj@hotmail.com>
2025-04-22 16:24:50 -0400

    ejercicios con for

---
+++ clase2.py
@@ -0,0 +1,43 @@
+# for i in range(1, 11):
+#     print("Esta es la tabla del ", i)
+#     for j in range(1, 11):
+#         print(i, "x", j, "=", i * j)
+
+# cant=int(input("Ingrese cantidad de repeticiones: "))
+# for i in range(cant):
+#     print("Esta es la tabla del ", i)
+#     for j in range(1, 11):
+#         print(i, "x", j, "=", i * j)
+
+# nombre=str(input("Ingrese su nombre: "))
+# edad=int(input("Ingrese su edad: "))
+# print(F"Hola {nombre} su edad es {edad}")
+
+# num=int(input("Ingrese un numero para ver su tabla de multiplicar: "))
+
+# for i in range(1,11):
+#     print(i, "x", num, "=", i*num)
+
+# cant=int(input("Ingrese cantidad de notas"))
+# total=0
+
+# for i in range(cant):
+#     print("Ingrese la nota ", i+1)
+#     nota=float(input())
+#     total=total+nota
+# prom=total/cant
+# print(F"Tu promedio es: {round(prom)}")
+
+# if prom >= 4:
+#     print("Aprobado")
+# else:
+#     print("Reprobado")
+
+# clave="1234"
+
+# passw=input("Ingrese la clave: ")
+# if passw == clave:
+#     print("Bienvenido al sistema")
+# else:
+#     print("Clave incorrecta")
+
